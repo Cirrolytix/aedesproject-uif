22526c8a51485ecad2b53237f027a79fb3b12c03
fstayco <56645234+fstayco@users.noreply.github.com>
2023-09-12 23:12:13 +0800

    Modified the setup_project_structure utility function
    Modified the setup_project_structure utility function to create the model, data, and processed directories directly to the specified destination_path without the aedesproject-uif-main prefix.

--- src/aedesproject_uif/__init__.py
+++ src/aedesproject_uif/__init__.py
@@ -3,6 +3,12 @@ import zipfile
 import io
 import os
 
+import requests
+import zipfile
+import io
+import os
+import shutil
+
 def setup_project_structure(destination_path):
     """
     Download and extract the model, processed, and data directories 
@@ -16,12 +22,25 @@ def setup_project_structure(destination_path):
     response = requests.get(repo_url)
     with zipfile.ZipFile(io.BytesIO(response.content)) as z:
         for file_info in z.infolist():
-            # Create model and processed directories without their contents
-            if file_info.filename in ["aedesproject-uif-master/model/", "aedesproject-uif-master/processed/"]:
-                os.makedirs(os.path.join(destination_path, file_info.filename), exist_ok=True)
-            
-            # Extract only the INFORM_DF_Variables.csv file from the data directory
-            elif file_info.filename == "aedesproject-uif-master/data/INFORM_DF_Variables.csv":
-                z.extract(file_info, destination_path)
+            # Check if the file is in one of the desired directories
+            if file_info.filename.startswith("aedesproject-uif-master/model/") or \
+               file_info.filename.startswith("aedesproject-uif-master/processed/") or \
+               file_info.filename == "aedesproject-uif-master/data/INFORM_DF_Variables.csv":
+                
+                # Remove the 'aedesproject-uif-master/' prefix from the filename
+                new_filename = file_info.filename.replace("aedesproject-uif-master/", "")
+                target_path = os.path.join(destination_path, new_filename)
+                
+                # If it's a directory, just create it and continue to the next item
+                if target_path.endswith('/'):
+                    os.makedirs(target_path, exist_ok=True)
+                    continue
+                
+                # Create necessary directories for the target path
+                os.makedirs(os.path.dirname(target_path), exist_ok=True)
+                
+                # Extract the file to the target path
+                with z.open(file_info) as source, open(target_path, "wb") as target:
+                    shutil.copyfileobj(source, target)
 
     print(f"Project structure set up at {destination_path}")
